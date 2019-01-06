ef3faaeff6a181c7087c310749a905732b136128
John Cao <johncy.cao@gmail.com>
2019-01-05 23:24:48 -0500

    fix error

--- config.py
+++ config.py
@@ -6,4 +6,4 @@ class Config(object):
 	# location of the application's database
 	SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI') or 'sqlite:///' + os.path.join(basedir, 'app.db')
 	SQLALCHEMY_TRACK_MODIFICATIONS = False	
-	LOG_TO_STDOUT = os.environ,get('LOG_TO_STDOUT')
+	LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
